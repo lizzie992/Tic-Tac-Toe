f5e04c44952fd23dfa20a4a023fb20deb52be28f
lizzie992 <gulyaskata99@gmail.com>
2024-08-25 22:15:52 +0200

    Add project files.

---
+++ Tic Tac Toe/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Tic_Tac_Toe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //define the grid
+            const int sizeOfGrid = 3;
+            string[,] grid = new string[sizeOfGrid, sizeOfGrid];
+
+            const string UNDERLINE = " _ ";
+            const string PLAYERCHARACTER = " X ";
+            const string COMPUTERCHARACTER = " O ";
+
+            //fill it up with empty _ characters so it can be visualized as we fill it out one by one
+            for (int x = 0; x < sizeOfGrid; x++)
+            {
+                for (int y = 0; y < sizeOfGrid; y++)
+                {
+                    grid[x, y] = UNDERLINE;
+                }
+            }
+
+            //and print it with the method - at first it will be completely empty
+            UserInterface.PrintGrid(grid);
+            int numberOfRow = 0;
+            int numberOfColumn = 0;
+
+            for (int i = 0; i < sizeOfGrid*sizeOfGrid; i++)
+            {
+                UserInterface.PlayerMoveRow();
+                UserInterface.PlayerMoveColumn();
+                grid[numberOfRow, numberOfColumn] = PLAYERCHARACTER;
+                UserInterface.PrintGrid(grid);
+
+
+
+            }
+        }
+    }
+}
---
+++ Tic Tac Toe/UserInterface.cs
@@ -0,0 +1,53 @@
+﻿namespace Tic_Tac_Toe
+{
+    public static class UserInterface
+    {
+        //method for printing the current state of the grid:
+
+        /// <summary>
+        /// Prints the current state of the grid
+        /// </summary>
+        /// <param name="grid">add the name of the grid</param>
+        public static void PrintGrid(string[,] grid)
+        {
+            const int sizeOfGrid = 3;
+            for (int x = 0; x < sizeOfGrid; x++)
+            {
+                for (int y = 0; y < sizeOfGrid; y++)
+                {
+                    Console.Write(grid[x, y]);
+                }
+                Console.Write($"\r\n");
+            }
+        }
+
+        //we need the input from the player regarding where they want to play the next character (2 methods, 1 for the row and 1 for the column, so we can return 1-1 int)
+
+        /// <summary>
+        /// Defines the number of rows for the next character in the grid
+        /// </summary>
+        /// <returns>Number of rows for the next character in the grid as an int</returns>
+        public static int PlayerMoveRow()
+        {
+            const string INSTUCTION_FOR_ROW = "Please give me the number of row where you want to play your next move (1-3 from top to bottom):";
+            Console.WriteLine(INSTUCTION_FOR_ROW);
+            int numberOfRow = Convert.ToInt32(Console.ReadLine());
+            numberOfRow--;
+            return numberOfRow;
+        }
+
+        /// <summary>
+        /// Defines the number of columns for the next character in the grid
+        /// </summary>
+        /// <returns>Number of columns for the next character in the grid as an int</returns>
+        public static int PlayerMoveColumn()
+        {
+            const string INSTUCTION_FOR_COLUMN = "Please give me the number of column where you want to play your next move (1-3 from left to right):";
+            Console.WriteLine(INSTUCTION_FOR_COLUMN);
+            int numberOfColumn = Convert.ToInt32(Console.ReadLine());
+            numberOfColumn--;
+            return numberOfColumn;
+        }
+    }
+}
+
